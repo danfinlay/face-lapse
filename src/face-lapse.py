0cbbb71f5a81097a08d62d8639e4a764e5b07dc4
Dan Finlay <dan@danfinlay.com>
2023-01-13 15:23:23 -0800

    Add new combination by the ai

---
+++ src/face-lapse.py
@@ -0,0 +1,57 @@
+import argparse
+import cv2
+import os
+import math
+import numpy as np
+from moviepy.editor import ImageSequenceClip
+
+def align_pupils(image, face_detector, eye_cascade):
+    (h, w) = image.shape[:2]
+    # Pass the image through the face detector
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    face_detector.setInput(blob)
+    detections = face_detector.forward()
+
+    # Iterate through the detections and align the pupils
+    for i in range(0, detections.shape[2]):
+        # Get the confidence of the detection
+        confidence = detections[0, 0, i, 2]
+
+        # If the detection is above a certain confidence threshold
+        if confidence > 0.5:
+            # Get the coordinates of the bounding box
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # Get the face ROI
+            face_roi = image[startY:endY, startX:endX]
+
+            # Align the pupils
+            gray = cv2.cvtColor(face_roi, cv2.COLOR_BGR2GRAY)
+            gray = cv2.equalizeHist(gray)
+
+            # Detect eyes
+            eyes = eye_cascade.detectMultiScale(gray)
+
+            if len(eyes) == 2:
+                # Get the coordinates of the eyes
+                left_eye = eyes[0]
+                right_eye = eyes[1]
+                if left_eye[0] > right_eye[0]:
+                    left_eye, right_eye = right_eye, left_eye
+
+                # Get the center of the eyes
+                left_eye_center = (left_eye[0] + left_eye[2] // 2, left_eye[1] + left_eye[3] // 2)
+                right_eye_center = (right_eye[0] + right_eye[2] // 2, right_eye[1] + right_eye[3] // 2)
+
+                # Get the angle between the eyes
+                dy = right_eye_center[1] - left_eye_center[1]
+                dx = right_eye_center[0] - left_eye_center[0]
+                angle = math.atan2(dy, dx) * 180.0 / math.pi
+
+                # Get the rotation matrix
+                rot_mat = cv2.getRotationMatrix2D(left_eye_center, angle, 1.0)
+
+                # Rotate the image
+                result = cv2.warpAffine(face_roi, rot_mat, (face_roi.shape[1], face_roi.shape[0]), flags=cv2.INTER_LINEAR)
+                face_roi = result

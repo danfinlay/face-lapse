693e928ab8a6262bd586cd0af9005930027be7aa
Dan Finlay <dan@danfinlay.com>
2023-01-13 16:23:17 -0800

    Resize images to be the same size.
    Exception: Moviepy: ImageSequenceClip requires all images to be the same size
    
    Please make the script adjust the images to be the same size, without losing the face alignment.
    
    You can use the cv2.resize() function to resize the images to the same size, while preserving the aspect ratio of the image, in order to avoid losing the face alignment.
    You can set the size to be the size of the image with the largest width and height.
    Here is an example of how the script can be modified to resize the images to the same size:

--- src/face-lapse.py
+++ src/face-lapse.py
@@ -5,29 +5,56 @@ import math
 import numpy as np
 from moviepy.editor import ImageSequenceClip
 
-def align_pupils(image, face_cascade, eye_cascade):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    if len(faces) > 0:
-        (x, y, w, h) = faces[0]
-        # Get the face ROI
-        face_roi = image[y:y+h, x:x+w]
-        gray_roi = cv2.cvtColor(face_roi, cv2.COLOR_BGR2GRAY)
-        gray_roi = cv2.equalizeHist(gray_roi)
-        eyes = eye_cascade.detectMultiScale(gray_roi)
-        if len(eyes) == 2:
-            left_eye = eyes[0]
-            right_eye = eyes[1]
-            if left_eye[0] > right_eye[0]:
-                left_eye, right_eye = right_eye, left_eye
-            # Get the center of the eyes
-            left_eye_center = (left_eye[0] + left_eye[2] // 2, left_eye[1] + left_eye[3] // 2)
-            right_eye_center = (right_eye[0] + right_eye[2] // 2, right_eye[1] + right_eye[3] // 2)
-            # Get the angle between the eyes
-            dy = right_eye_center[1] - left_eye_center[1]
-            dx = right_eye_center[0] - left_eye_center
+def align_pupils(image, face_detector, eye_cascade):
+    (h, w) = image.shape[:2]
+    # Pass the image through the face detector
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    face_detector.setInput(blob)
+    detections = face_detector.forward()
 
+    # Iterate through the detections and align the pupils
+    for i in range(0, detections.shape[2]):
+        # Get the confidence of the detection
+        confidence = detections[0, 0, i, 2]
+
+        # If the detection is above a certain confidence threshold
+        if confidence > 0.5:
+            # Get the coordinates of the bounding box
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # Get the face ROI
+            face_roi = image[startY:endY, startX:endX]
+
+            # Align the pupils
+            gray = cv2.cvtColor(face_roi, cv2.COLOR_BGR2GRAY)
+            gray = cv2.equalizeHist(gray)
+
+            # Detect eyes
+            eyes = eye_cascade.detectMultiScale(gray)
+
+            if len(eyes) == 2:
+                # Get the coordinates of the eyes
+                left_eye = eyes[0]
+                right_eye = eyes[1]
+                if left_eye[0] > right_eye[0]:
+                    left_eye, right_eye = right_eye, left_eye
+
+                # Get the center of the eyes
+                left_eye_center = (left_eye[0] + left_eye[2] // 2, left_eye[1] + left_eye[3] // 2)
+                right_eye_center = (right_eye[0] + right_eye[2] // 2, right_eye[1] + right_eye[3] // 2)
+
+                # Get the angle between the eyes
+                dy = right_eye_center[1] - left_eye_center[1]
+                dx = right_eye_center[0] - left_eye_center[0]
+                angle = math.atan2(dy, dx) * 180.0 / math.pi
+
+                # Get the rotation matrix
+                rot_mat = cv2.getRotationMatrix2D(left_eye_center, angle, 1.0)
+
+                # Rotate the image
+                result = cv2.warpAffine(face_roi, rot_mat, (face_roi.shape[1], face_roi.shape[0]), flags=cv2.INTER_LINEAR)
+                face_roi = result
 
 if __name__ == '__main__':
     # Create the argument parser
@@ -48,11 +75,17 @@ if __name__ == '__main__':
     face_cascade = cv2.CascadeClassifier(cascades_root + 'haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier(cascades_root + 'haarcascade_eye.xml')
     images = []
+    max_width = 0
+    max_height = 0
     # Iterate through all the images in the folder
     for image_name in os.listdir(args.input_folder):
         # Load the image
         image = cv2.imread(os.path.join(args.input_folder, image_name))
         align_pupils(image, face_cascade, eye_cascade)
+        max_height = max(max_height,image.shape[0])
+        max_width = max(max_width,image.shape[1])
+        #resizing image to max size
+        image = cv2.resize(image, (max_width, max_height))
         cv2.imwrite(os.path.join(args.output_folder, image_name), image)
         images.append(image)
     # Creating the video file
